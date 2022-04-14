a06d26379066f0245177c36341d25afeca4e09ab
EsauM10 <esau2096@gmail.com>
2022-04-14 16:07:19 -0300

    Add iqoptionapi dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tradingbot",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     description="A scalable bot where you can create custom trading strategies",
     long_description="A scalable bot where you can create custom trading strategies",
@@ -11,7 +11,10 @@ setup(
     url="https://github.com/EsauM10/tradingbot",
     author="Esaú Mascarenhas",
     author_email="esaumasc@gmail.com",
-    install_requires=["numpy"],
+    install_requires=[
+        "numpy",
+        "iqoptionapi @ https://github.com/iqoptionapi/iqoptionapi/tarball/master#egg=iqoptionapi"
+    ],
     classifiers=[
         "Intended Audience :: Financial and Insurance Industry",
         "Programming Language :: Python",
