2770f911737bd80a7d3faaf06f2e37cc2bb163dd
EsauM10 <esau2096@gmail.com>
2022-04-22 14:55:42 -0300

    Refactor class TradingBotBase

--- trading/__init__.py
+++ trading/__init__.py
@@ -1,9 +1,16 @@
 from abc import ABC, abstractmethod
+from trading.exchanges import Exchange
+from trading.setup import TradingSetup
+from trading.strategies import TradingStrategy
 from trading.util import Action, Transaction
 
 
 class TradingBotBase(ABC):
-    
+    def __init__(self, exchange: Exchange, setup: TradingSetup, strategy: TradingStrategy):
+        self.exchange      = exchange
+        self.setup         = setup
+        self.strategy      = strategy
+        
     @abstractmethod
     def perform_transaction(self, action: Action) -> Transaction:
         pass
