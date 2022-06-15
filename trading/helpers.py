688e8af17688f650ae0ded4226a6b709e3f1a68d
EsauM10 <esau2096@gmail.com>
2022-06-15 17:04:10 -0300

    Add get_max_price and get_min_price

--- trading/helpers.py
+++ trading/helpers.py
@@ -2,6 +2,8 @@ import numpy as np
 from numpy import ndarray
 from trading.util import Candle
 
+PRICE_KEYS = ['high', 'low', 'open', 'close']
+
 def filter_data_by(data: list[Candle], key:str) -> ndarray:
     '''Filtra os dados de uma lista de Candle por uma key especifica''' 
     return np.array([
@@ -15,3 +17,19 @@ def get_dataset(candles: list[Candle], keys: list[str]) -> dict[str, ndarray]:
         key: filter_data_by(candles, key) 
         for key in keys
     }
+
+
+def get_max_price(candles: list[Candle], price_type: str) -> float:
+    if(not price_type in PRICE_KEYS):
+        raise KeyError(f'Informe uma key valida: {PRICE_KEYS}')
+
+    data = filter_data_by(data=candles, key=price_type)
+    return float(data.max())
+
+
+def get_min_price(candles: list[Candle], price_type: str) -> float:
+    if(not price_type in PRICE_KEYS):
+        raise Exception(f'Informe uma key valida: {PRICE_KEYS}')
+    
+    data = filter_data_by(data=candles, key=price_type)
+    return float(data.min())
